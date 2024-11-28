0a178f7302ef1270de92750ac72f4ca4248645b5
sebastian <sebastian.calle@globant.com>
2024-11-28 14:38:44 -0500

    removing files

--- streamsExamples/transformStream.js
+++ streamsExamples/transformStream.js
@@ -1,40 +1,5 @@
-const { Transform, Readable, Writable } = require('node:stream');
+const { Transform } = require('node:stream');
 const { pipeline } = require('node:stream/promises');
-const fs = require('node:fs');
-
-const file = fs.createWriteStream('./transformStdIn.txt');
-
-const readableStream = new Readable({
-    read() {},
-});
-
-const writableStream = new Writable({
-    write(chunk, encoding, callback) {
-        if (chunk.toString() === 'exit\n') {
-            file.end();
-            this.end();
-        } else {
-            file.write(chunk.toString().toUpperCase());
-        }
-    },
-});
-
-writableStream.on('pipe', () => {
-    console.log('piping to the writable stream')
-});
-
-readableStream.on('end', () => {
-    console.log('input captured successfuly')
-});
-
-// process.stdin.on('data', function(chunk) {
-//     if (chunk.toString() == 'exit\n') {
-//         process.stdin.destroy();
-//         readableStream.push(null);
-//     } else {
-//         readableStream.push(chunk.toString());
-//     }
-// });
 
 const transformStream = new Transform({
     transform(chunk, encoding, callback) {
